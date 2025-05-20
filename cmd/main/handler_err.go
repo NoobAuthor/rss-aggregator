9245175556bb6e587ff3e9f93280eaebc6b36b14
NoobAuthor <igor21.assis@outlook.com>
2025-05-20 10:37:12 -0400

    refactor: finished updating

--- cmd/main/handler_err.go
+++ cmd/main/handler_err.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/sirupsen/logrus"
 )
 
 func HandlerError(w http.ResponseWriter, r *http.Request) {
-	logrus.Error("HandlerError endpoint was called")
-	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	respondWithError(w, http.StatusInternalServerError, "StatusInternalServerError")
 }
